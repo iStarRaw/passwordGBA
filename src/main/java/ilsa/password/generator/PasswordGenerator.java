c63423d8c6dbacb6a3be319d7cf23f5cb4b55c2e
Ilsa <35494284+iStarRaw@users.noreply.github.com>
2018-10-23 12:18:24 +0200

    changed from array to arraylist

--- src/main/java/ilsa/password/generator/PasswordGenerator.java
+++ src/main/java/ilsa/password/generator/PasswordGenerator.java
@@ -1,48 +1,43 @@
 package ilsa.password.generator;
 
-
 import ilsa.password.models.CharacterBox;
 import ilsa.password.models.Password;
 
-public class PasswordGenerator  {
+public class PasswordGenerator {
 
 	private Password password;
 	private CharacterBox cbox;
-	
 
 	public PasswordGenerator(int length) {
 		password = new Password(length);
 		cbox = new CharacterBox();
-		
+
 		createPassword();
 	}
 
-	
 	private void createPassword() {
-		for (int i = 0; i < password.getLength(); i++) {
+		for (int i = 0; i < password.getPassword().size(); i++) {
 			addChar(i);
 
-			System.out.println(password.getPassword()[i]);
+			System.out.println(password.getPassword().get(i));
 		}
 
 	}
-	
-	
-	
+
 	private void addChar(int index) {
 
 		if (index < 2) {
 			// pick from all possibilities
-			password.getPassword()[index] = cbox.generateFromAll();
+			password.getPassword().add(cbox.generateFromAll());
 		}
 
 		if (!twoBeforeSame() && !duplicatesExist()) {
-			//pick from all possibilities
-			password.getPassword()[index] = cbox.generateFromAll();
+			// pick from all possibilities
+			password.getPassword().add(cbox.generateFromAll());
 //		} else if (duplicatesExist()) {
 //			//pick from all without the duplicate
 			int duplicate = getDuplicate();
-			password.getPassword()[index] = cbox.generateWithout(duplicate);
+			password.getPassword().add(cbox.generateWithout(duplicate));
 //		}
 //		
 //		if (twoBeforeSame && threeBeforeSame()) {
@@ -50,7 +45,7 @@ public class PasswordGenerator  {
 //				//pick from other sort without the duplicate
 //			}
 //			//pick from all possibilities
-//			password.getPassword()[index] = cbox.generateFromAll();
+//			password.getPassword().add(cbox.generateFromAll());
 //		}
 //		
 //		//condition that two of the same type are not allowed together
@@ -70,33 +65,46 @@ public class PasswordGenerator  {
 //			//pick another
 //		}
 
-		password.getPassword()[index] = cbox.generateFromAll();
-		
+			password.getPassword().add(cbox.generateFromAll());
+
 		}
-		
+
 	}
 
-		private int getDuplicate() {
-			// TODO Auto-generated method stub
-			return 0;
+	private int getDuplicate() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < password.getLength(); i++) {
+
 		}
+		return 0;
+	}
 
-
-		private boolean duplicatesExist() {
-			// TODO Auto-generated method stub
+	private boolean duplicatesExist() {
+		// TODO Auto-generated method stub
+		if (password.getPassword().size() < 2) {
 			return false;
 		}
-
-
-		private boolean twoBeforeSame() {
-			// TODO Auto-generated method stub
-			return false;
+		
+		int occurrences = 0;
+		for (int i = 0; i < password.getLength() - 1; i++) {
+			if (password.getPassword().get(i) == password.getPassword().get(i + 1)) {
+				occurrences++;
+			}
+			if (occurrences == 2) {
+				return true;
+			}
 		}
+		return false;
+	}
+	
 
-	
-	
+	private boolean twoBeforeSame() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < password.getLength(); i++) {
+			
+		}
+		
+		return false;
+	}
 
 }
-
-
-	
--- src/main/java/ilsa/password/models/Password.java
+++ src/main/java/ilsa/password/models/Password.java
@@ -1,28 +1,32 @@
 package ilsa.password.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Password {
 	private int length;
-	private int[] password;
+	private List<Integer> password;
 
 
 	public Password(int length) {
 		this.length = length;
-		this.password = new int[length];
+		this.password = new ArrayList<>();
 	}
 
-	public int getLength() {
-		return length;
-	}
-
-	public int[] getPassword() {
+	public List<Integer> getPassword() {
 		return password;
 	}
 	
 	
+	
+	public int getLength() {
+		return length;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder passwordString = new StringBuilder();
-		for (int i = 0; i < password.length; i++) {
+		for (int i = 0; i < password.size(); i++) {
 			passwordString.append(Character.toString((char) i));
 		}
 		return passwordString.toString();
