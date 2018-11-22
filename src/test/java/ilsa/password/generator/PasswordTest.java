bcc1d9bcf6185088d48aa30ce4cf36d6e8be47b3
Ilsa <idejager@hotmail.com>
2018-11-22 09:51:52 +0100

    toevoegen testAddThisChar

--- src/test/java/ilsa/password/generator/PasswordTest.java
+++ src/test/java/ilsa/password/generator/PasswordTest.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 class PasswordTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAddThisChar() {
+		Password pw = new Password(8);
+		
+		char thisChar = 'k';	
+		char otherChar = '0';
+		pw.addThisChar(thisChar);
+		pw.addThisChar(otherChar);
+		
+		char actual = pw.getPassword().get(0);
+		char otherActual = pw.getPassword().get(1);
+		
+		assertEquals(thisChar, actual);
+		assertEquals(otherChar, otherActual);
+	
 	}
 
 }
