59eb0c71c4a222f9be98e1c3928ce4ee58e49bbb
Ilsa <idejager@hotmail.com>
2018-11-22 10:59:03 +0100

    added comments in Password

--- src/main/java/ilsa/password/generator/Password.java
+++ src/main/java/ilsa/password/generator/Password.java
@@ -3,7 +3,10 @@ package ilsa.password.generator;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * @author Dictu
+ *
+ */
 public class Password {
 	private int length;
 	private List<Character> password;
@@ -20,43 +23,68 @@ public class Password {
 	public int getLength() {
 		return length;
 	}
-	
+
+	/**
+	 * Adds a char to the password.
+	 * 
+	 * @param thisChar
+	 */
 	public void addThisChar(char thisChar) {
 		this.password.add(thisChar);
 	}
 
+	/**
+	 * Checks if the values of the number of indexes given, are of the same sort.
+	 * 
+	 * @param amount
+	 * @return boolean
+	 */
 	public boolean areSameSort(int amount) {
 		int limitIndex = password.size() - amount;
 		int lastIndex = password.size() - 1;
-		
+
 		if (lastIndex == 1 && amount == 3) {
 			return false;
 		}
-		
+
 		for (int i = lastIndex; i > limitIndex; i--) {
 			int beforeLastIndex = lastIndex - 1;
 			char lastChar = password.get(lastIndex);
 			char beforeLastChar = password.get(beforeLastIndex);
 			boolean result = isSameSort(lastChar, beforeLastChar);
-			
+
 			if (!result) {
 				return false;
-			} 
+			}
 
 			lastIndex--;
 
 			if (lastIndex == 0 || beforeLastIndex == 0) {
 				return result;
-			}			
+			}
 		}
 		return true;
 	}
 
-	public boolean isSameSort(char lastChar, char beforeLastChar) {
+	/**
+	 * Checks if two chars are of the same sort (digit and alphabetic). Symbol is
+	 * not of importance and not checked upon.
+	 * 
+	 * @param lastChar
+	 * @param beforeLastChar
+	 * @return boolean
+	 */
+	private boolean isSameSort(char lastChar, char beforeLastChar) {
 		return ((Character.isDigit(lastChar) && Character.isDigit(beforeLastChar))
 				|| (Character.isAlphabetic(lastChar) && Character.isAlphabetic(beforeLastChar)));
 	}
 
+	/**
+	 * Checks if the value of the last index is a duplication of another value in
+	 * the password.
+	 * 
+	 * @return boolean
+	 */
 	public boolean lastIsDuplicate() {
 		if (password.size() < 2) {
 			return false;
@@ -72,10 +100,22 @@ public class Password {
 
 	}
 
+	/**
+	 * If last char is a duplicate, it returns this value.
+	 * 
+	 * @return char
+	 */
 	public char getDuplicate() {
 		return password.get(password.size() - 1);
 	}
 
+	/**
+	 * Gets name of the sort (Digit/Letter/Other) of the char given and returns this
+	 * in a String.
+	 * 
+	 * @param index
+	 * @return String
+	 */
 	public String getCharSort(int index) {
 		char toCheck = password.get(index);
 
@@ -88,6 +128,13 @@ public class Password {
 
 	}
 
+	/**
+	 * Checks if the two last chars are in sequence (in case they are both numbers
+	 * or digits). For example in the case of ab, ba, 12, or 21 false is being
+	 * returned.
+	 * 
+	 * @return boolean
+	 */
 	public boolean isSequence() {
 		if (password.size() < 1) {
 			return false;
@@ -112,7 +159,18 @@ public class Password {
 
 	}
 
+	/**
+	 * If the last two chars(the int representations) are a sequence, the next int
+	 * in sequence will be given back as a char. 0 is returned if no
+	 * forbiddenChar exists.
+	 * 
+	 * @return char
+	 */
 	public char getForbiddenChar() {
+		if (password.size() < 1) {
+			return 0;
+		}
+
 		int lastCharInt = (int) password.get(password.size() - 1);
 		int beforeLastCharInt = (int) password.get(password.size() - 2);
 
@@ -162,6 +220,11 @@ public class Password {
 
 	}
 
+	/**
+	 * Password will be generated with the length given.
+	 * @param length
+	 * @return List
+	 */
 	public static Password generate(int length) {
 		PasswordGenerator pg = new PasswordGenerator(length);
 		return pg.getPassword();
--- src/test/java/ilsa/password/generator/PasswordTest.java
+++ src/test/java/ilsa/password/generator/PasswordTest.java
@@ -8,49 +8,119 @@ class PasswordTest {
 
 	@Test
 	void testAddThisChar() {
-		Password pw = new Password(8);
-
+		Password pw = new Password(2);
 		char thisChar = 'k';
 		char otherChar = '0';
+		
 		pw.addThisChar(thisChar);
 		pw.addThisChar(otherChar);
 
 		char actual = pw.getPassword().get(0);
 		char otherActual = pw.getPassword().get(1);
-
 		assertEquals(thisChar, actual);
 		assertEquals(otherChar, otherActual);
 
 	}
+	
+//	@Test
+//	void testIsSameSortTrue() {
+//		Password pw = new Password(2);
+//		char thisChar = '4';
+//		char otherChar = '0';
+//		
+//		boolean actual = pw.isSameSort(thisChar, otherChar);
+//		assertTrue(actual);
+//	}
+//
+//	@Test
+//	void testIsSameSortFalse() {
+//		Password pw = new Password(2);
+//		char thisChar = 'k';
+//		char otherChar = '0';
+//		
+//		boolean actual = pw.isSameSort(thisChar, otherChar);
+//		assertFalse(actual);
+//		
+//	}
 
 	@Test
-	void testIsSameSortFalse() {
-		Password pw = new Password(4);
-
-		char thisChar = 'k';
-		char otherChar = '0';
+	void testAreSameSortTrue() {
+		Password pw = new Password(6);
+		char char1 = '@';
+		char char2 = 'a';
+		char char3 = 'r';
+		char char4 = '3';
+		char char5 = '4';
+		char char6 = '0';
 		
-		boolean actual = pw.isSameSort(thisChar, otherChar);
-
-		assertFalse(actual);
+		pw.addThisChar(char1);
+		pw.addThisChar(char2);
+		pw.addThisChar(char3);
+		pw.addThisChar(char4);
+		pw.addThisChar(char5);
+		pw.addThisChar(char6);
 		
+		boolean actual = pw.areSameSort(3);
+		assertTrue(actual);
+
 	}
 	
 	@Test
-	void testIsSameSortTrue() {
-		Password pw = new Password(4);
-
-		char thisChar = '4';
-		char otherChar = '0';
+	void testAreSameSortFalse() {
+		Password pw = new Password(6);
+		char char1 = '@';
+		char char2 = 'a';
+		char char3 = 'r';
+		char char4 = 'p';
+		char char5 = '4';
+		char char6 = ';';
 		
-		boolean actual = pw.isSameSort(thisChar, otherChar);
+		pw.addThisChar(char1);
+		pw.addThisChar(char2);
+		pw.addThisChar(char3);
+		pw.addThisChar(char4);
+		pw.addThisChar(char5);
+		pw.addThisChar(char6);
+		
+		boolean actual = pw.areSameSort(3);
+		assertFalse(actual);
 
+	}
+	
+	@Test
+	void testLastIsDuplicateTrue() {
+		Password pw = new Password(4);
+		char char1 = 'k';
+		char char2 = '0';
+		char char3 = 'a';
+		char char4 = 'a';
+		
+		pw.addThisChar(char1);
+		pw.addThisChar(char2);
+		pw.addThisChar(char3);
+		pw.addThisChar(char4);
+		
+		boolean actual = pw.lastIsDuplicate();
 		assertTrue(actual);
 	}
 
 	@Test
-	void testAreSameSort() {
-
+	void testLastIsDuplicateFalse() {
+		Password pw = new Password(4);
+		char char1 = 'k';
+		char char2 = '0';
+		char char3 = '@';
+		char char4 = 'a';
+		
+		pw.addThisChar(char1);
+		pw.addThisChar(char2);
+		pw.addThisChar(char3);
+		pw.addThisChar(char4);
+		
+		boolean actual = pw.lastIsDuplicate();
+		assertFalse(actual);
 	}
-
+	
+	
+	
 }
