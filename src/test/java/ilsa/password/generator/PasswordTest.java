3501bfb0638f0ecee4d0049ed6320a558101cb21
Ilsa <idejager@hotmail.com>
2018-11-22 10:02:58 +0100

    isSameSortTrue & isSameSortFalse toegevoegd

--- src/test/java/ilsa/password/generator/PasswordTest.java
+++ src/test/java/ilsa/password/generator/PasswordTest.java
@@ -9,18 +9,48 @@ class PasswordTest {
 	@Test
 	void testAddThisChar() {
 		Password pw = new Password(8);
-		
-		char thisChar = 'k';	
+
+		char thisChar = 'k';
 		char otherChar = '0';
 		pw.addThisChar(thisChar);
 		pw.addThisChar(otherChar);
-		
+
 		char actual = pw.getPassword().get(0);
 		char otherActual = pw.getPassword().get(1);
-		
+
 		assertEquals(thisChar, actual);
 		assertEquals(otherChar, otherActual);
+
+	}
+
+	@Test
+	void testIsSameSortFalse() {
+		Password pw = new Password(4);
+
+		char thisChar = 'k';
+		char otherChar = '0';
+		
+		boolean actual = pw.isSameSort(thisChar, otherChar);
+
+		assertFalse(actual);
+		
+	}
 	
+	@Test
+	void testIsSameSortTrue() {
+		Password pw = new Password(4);
+
+		char thisChar = '4';
+		char otherChar = '0';
+		
+		boolean actual = pw.isSameSort(thisChar, otherChar);
+
+		assertTrue(actual);
+	}
+
+	@Test
+	void testAreSameSort() {
+
 	}
 
 }
